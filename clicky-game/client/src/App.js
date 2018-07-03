b13e56470ed6b0dc7251a67f4ded93d7ec729322
Adam Miner <miner.adam1@hotmail.com>
2018-07-03 10:05:10 -0600

    Progress

--- clicky-game/client/src/App.js
+++ clicky-game/client/src/App.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ClickyCard from "./components/ClickyCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Counter from './components/Counter';
+import clicky from "./clicky.json";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    clicky
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Wrapper>
+        <Title>LotR Clicky!</Title>
+        <Counter />
+        {this.state.clicky.map(click => (
+          <ClickyCard
+            removeClicky={this.removeClicky}
+            id={click.id}
+            key={click.id}
+            image={click.image}
+          />
+        ))}
+      </Wrapper>
+    )
   }
 }
 
